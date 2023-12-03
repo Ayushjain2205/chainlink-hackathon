4e819f955c6fc5433ddc75e5e496aee5241ba185
Ayush Jain <ayushjain2205@gmail.com>
2023-12-03 22:55:42 +0530

    Make px py part of layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
       <div className="flex flex-col justify-center p-[26px] h-[80px] bg-[#DCD2C7]">
         <span className="font-[700] text-[24px]"> Navbar</span>
       </div>
-      {children}
+      <div className="px-[30px] py-[32px]"> {children}</div>
     </div>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,7 @@ import Layout from "../components/Layout/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div className="px-[30px] py-[32px]">
-        <h1>hELLO</h1>
-      </div>
+      <h1>hELLO</h1>
     </Layout>
   );
 }
