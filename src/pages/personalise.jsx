9da8f131b13e3e1a2788a5841c38fd6b7c95094a
Ayush Jain <ayushjain2205@gmail.com>
2023-12-04 13:02:45 +0530

    Add side panel

--- src/pages/personalise.jsx
+++ src/pages/personalise.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import SidePanel from "../components/Layout/SidePanel";
 
-const personalise = () => {
+const Personalise = () => {
   return (
     <Layout>
-      <div>Hello</div>
+      <div className="flex flex-row gap-[20px]">
+        <SidePanel />
+        <div className="flex flex-col">
+          <p className="text-[24px] font-[700]">PERSONALISE NINJA BOT</p>
+        </div>
+      </div>
     </Layout>
   );
 };
 
-export default personalise;
+export default Personalise;
