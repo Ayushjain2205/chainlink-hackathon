d1b000691d5475edcc0e7f06292a99a8583fd638
Ayush Jain <ayushjain2205@gmail.com>
2023-12-05 19:24:23 +0530

    Add bot card

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Layout = ({ children }) => {
   return (
     <div className="w-screen overflow-hidden">
       <div className="flex flex-col justify-center p-[26px] h-[80px] bg-[#DCD2C7]">
-        <span className="font-[700] text-[24px]"> Navbar</span>
+        <Link href="/">
+          <span className="font-[700] text-[24px]"> Navbar</span>
+        </Link>
       </div>
       <div className="px-[30px] py-[32px]"> {children}</div>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,36 @@
 import React, { useEffect } from "react";
 import Layout from "../components/Layout/Layout";
+import BotCard from "../components/UI/BotCard";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>hELLO</h1>
+      <div className="flex flex-col">
+        <h5 className="text-[48px] font-[700]">EXPLORE</h5>
+        <div className="flex flex-row mt-[43px] space-x-[22px] border-b border-[#DCD2C7] pb-[20px]">
+          <BotCard
+            title="NINJA TRADEBOT"
+            imageSrc="/images/ninja.png"
+            volume="1,690,850"
+            profit="$ 2011.08"
+          />
+          <BotCard
+            title="ASTRONAUT"
+            imageSrc="/images/astronaut.png"
+            volume="1,690,850"
+            profit="$ 2011.08"
+          />
+          <BotCard
+            title="COWBOY"
+            imageSrc="/images/cowboy.png"
+            volume="1,690,850"
+            profit="$ 2011.08"
+          />
+        </div>
+        <div className="flex flex-row mt-[43px] space-x-[22px]  pb-[20px]">
+          <div className="flex"></div>
+        </div>
+      </div>
     </Layout>
   );
 }
