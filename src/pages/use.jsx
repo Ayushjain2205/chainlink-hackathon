e754d930cff9c4b7f7ea5233b06d8721492138f0
Ayush Jain <ayushjain2205@gmail.com>
2023-12-06 11:54:59 +0530

    update top bar with login with telegram

--- src/pages/use.jsx
+++ src/pages/use.jsx
@@ -1,11 +1,19 @@
 import React, { useRef, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import axios from "axios";
 import dynamic from "next/dynamic";
 const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+import TelegramLoginButton, { TelegramUser } from "telegram-login-button";
 import Layout from "../components/Layout/Layout";
 import BotPanel from "../components/Layout/BotPanel";
 
 export default function Use() {
+  const router = useRouter();
+  const { id, first_name, last_name, username, photo_url, auth_date, hash } =
+    router.query;
+
+  const isLoggedIn = Boolean(id);
+
   const [series, setSeries] = useState([]);
   const [selectedCurrency, setSelectedCurrency] = useState("ethereum");
 
@@ -45,17 +53,29 @@ export default function Use() {
       <div className="flex flex-row">
         <BotPanel />
         <div className="w-full flex-shrink-0 ">
-          <div className="w-full  border border-[#DCD2C7]">
-            <select
-              value={selectedCurrency}
-              onChange={(e) => setSelectedCurrency(e.target.value)}
-              className="outline-none font-[700] text-grey text-[20px]"
-            >
-              <option value="ethereum">ETH</option>
-              <option value="usd-coin">USDC</option>
-              <option value="tether">USDT</option>
-              <option value="chainlink">LINK</option>
-            </select>
+          <div className=" flex flex-row h-[70px] w-full  border border-[#DCD2C7]">
+            <div className="w-[160px] px-[40px] py-[21px] items-center border-r">
+              <select
+                value={selectedCurrency}
+                onChange={(e) => setSelectedCurrency(e.target.value)}
+                className=" outline-none font-[700] text-grey text-[20px]"
+              >
+                <option value="ethereum">ETH</option>
+                <option value="usd-coin">USDC</option>
+                <option value="tether">USDT</option>
+                <option value="chainlink">LINK</option>
+              </select>
+            </div>
+            <div className="absolute top-[125px] right-[30px]">
+              {!isLoggedIn && (
+                <TelegramLoginButton
+                  botName="EntropyTesting_Bot"
+                  dataOnauth={(user) => console.log(user)}
+                  dataAuthUrl="https://trading-bott.netlify.app/"
+                  cornerRadius={5}
+                />
+              )}
+            </div>
           </div>
           <div className="">
             <div className="chart">
