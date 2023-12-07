fba60f054a2a90566ca171498b900428f55aedbd
Ayush Jain <ayushjain2205@gmail.com>
2023-12-07 15:27:10 +0530

    Fix Link bug

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,23 +6,21 @@ const Layout = ({ children }) => {
   return (
     <div className="max-w-screen overflow-hidden">
       <div className="flex flex-col justify-center p-[26px] h-[80px] border-b border-black ">
-        <Link href="/">
-          <div className="flex flex-row justify-between items-center">
-            <div className="flex flex-row gap-[20px] items-center ml-[180px]">
-              <img src="/bot.svg" alt="" />
-              <span className="font-[700] text-[24px]"> Botcoin</span>
-            </div>
-            <div className="flex flex-row gap-[30px] items-center">
-              <Link href="/" className="text-[20px] text-primary font-[500]">
-                Explore
-              </Link>
-              <Link href="/use" className="text-[20px] text-primary font-[500]">
-                My Bots
-              </Link>
-              <ConnectWallet className="connect-wallet" />
-            </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-row gap-[20px] items-center ml-[180px]">
+            <img src="/bot.svg" alt="" />
+            <span className="font-[700] text-[24px]"> Botcoin</span>
           </div>
-        </Link>
+          <div className="flex flex-row gap-[30px] items-center">
+            <Link href="/" className="text-[20px] text-primary font-[500]">
+              Explore
+            </Link>
+            <Link href="/use" className="text-[20px] text-primary font-[500]">
+              My Bots
+            </Link>
+            <ConnectWallet className="connect-wallet" />
+          </div>
+        </div>
       </div>
       <div className="px-[30px] py-[32px] overflow-hidden"> {children}</div>
     </div>
